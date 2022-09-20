1ed77f247e1177e52edf44113e3597b290ce5b39
webn3ewbie <96390676+webn3ewbie@users.noreply.github.com>
2022-09-20 16:58:59 -0400

    Update econterm.py

--- econterm.py
+++ econterm.py
@@ -5,6 +5,19 @@ from fredapi import Fred
 
 KEY = '9cbe93cd8132301fd46ad5e755944df0'
 fred = Fred(api_key=KEY)
+
+st.write("DB username:", st.secrets["db_username"])
+st.write("DB password:", st.secrets["db_password"])
+st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
+
+# And the root-level secrets are also accessible as environment variables:
+
+import os
+
+st.write(
+    "Has environment variables been set:",
+    os.environ["db_username"] == st.secrets["db_username"],
+
         
 econ_dictionary = {
     # GDP
