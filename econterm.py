d12a28238a2ead552c614121d6df498bc2ec5e7f
webn3ewbie <96390676+webn3ewbie@users.noreply.github.com>
2022-09-22 00:56:37 -0400

    Update econterm.py

--- econterm.py
+++ econterm.py
@@ -455,7 +455,7 @@ if major_selection == 'Commodities':
     date_condition = start_date < end_date
     
     st.subheader("Crude Oil Prices: West Texas Intermediate (WTI) - Cushing, Oklahoma")
-    wti = to_df('DCOILWTICO', start_date, end_date)
+    wti = to_df('DCOILWTICO'/10, start_date, end_date)
     show_chart(wti)
     
     st.subheader("Henry Hub Natural Gas Spot Price")
